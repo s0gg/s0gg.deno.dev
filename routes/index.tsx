e2cebf3c37a19f8437fbfe532b96e5c1453492c3
s0gg <mail@s0gg.net>
2024-03-13 23:37:51 +0900

    Initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,11 @@
+import { useSignal } from "@preact/signals";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  const count = useSignal(3);
+  return (
+    <div class="px-4 py-8 mx-auto">
+      <h1>s0gg</h1>
+    </div>
+  );
+}
