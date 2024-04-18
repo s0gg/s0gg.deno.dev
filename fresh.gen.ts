e1c5e17b7db4c2ed55d3b581c08680f3678e742c
s0gg <mail@s0gg.net>
2024-04-18 19:19:03 +0900

    Add dotfiles page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,6 +5,7 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
+import * as $dotfiles from "./routes/dotfiles.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $posts_slug_ from "./routes/posts/[slug].tsx";
@@ -17,6 +18,7 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
+    "./routes/dotfiles.tsx": $dotfiles,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/posts/[slug].tsx": $posts_slug_,
---
+++ routes/dotfiles.tsx
@@ -0,0 +1,23 @@
+export default function Dotfiles() {
+  return (
+    <>
+      <header>
+        <h1>dotfiles</h1>
+      </header>
+      <main>
+        <h2>My dotfiles</h2>
+        <h3>Terminal emulator</h3>
+        <p>
+          WezTerm{' '}
+          <a href="https://github.com/wez/wezterm" target="_blank">
+            GitHub
+          </a>
+          {' '}
+          <a href="https://github.com/s0gg/dotfiles/blob/main/.wezterm.lua" target="_blank">
+            my config
+          </a>
+        </p>
+      </main> 
+    </>
+  )
+}
