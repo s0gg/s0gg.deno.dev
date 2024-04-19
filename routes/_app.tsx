1ca86a615b8cc7dfaa952c9fd6dcf6a7be5c8f8d
s0gg <mail@s0gg.net>
2024-04-19 23:17:25 +0900

    Modify title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>s0gg.deno.dev</title>
+        <title>s0gg.net</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
