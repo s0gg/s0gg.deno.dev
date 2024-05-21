4711c32e9a443e2beb16b79bda40392730a10b81
s0gg <mail@s0gg.net>
2024-05-21 22:50:30 +0900

    Add contents

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,11 +6,19 @@ export default function Home() {
       </header>
       <main class="px-4 py-8 mx-auto">
         <section>
+          <h2>About me</h2>
+          <p>Software engineer.</p>
+        </section>
+        <section>
           <h2>Links</h2>
           <ul>
             <li>GitHub: <a href="https://github.com/s0gg" target="_blank">s0gg</a></li>
             <li>Blog: <a href="/posts">/posts</a></li>
           </ul>
+        </section>
+        <section>
+          <h2>Likes</h2>
+          <p>Rust, Lisp, CTF, Neovim, Emacs</p>
         </section>
       </main>
     </>
