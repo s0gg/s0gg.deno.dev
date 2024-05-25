cfa7d791629b3086f6222b932b59ebc831bc8561
s0gg <mail@s0gg.net>
2024-05-25 20:56:05 +0900

    Add meta tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,12 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>s0gg.net</title>
+        <meta name="description" content="s0gg's website" />
+        <meta property="og:url" content="https://s0gg.net" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="s0gg.net" />
+        <meta property="og:description" content="s0gg's website" />
+        <meta property="og:site_name" content="s0gg.net" />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
