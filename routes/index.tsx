dec25d79faccfa6c425909cc0cb71852b34a9037
s0gg <mail@s0gg.net>
2024-05-25 18:23:54 +0900

    Chagne style

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,26 +1,27 @@
 export default function Home() {
   return (
     <>
-      <header>
-        <h1>s0gg</h1>
-      </header>
-      <main class="px-4 py-8 mx-auto">
-        <section>
-          <h2>About me</h2>
-          <p>Software engineer.</p>
-        </section>
-        <section>
-          <h2>Links</h2>
-          <ul>
-            <li>GitHub: <a href="https://github.com/s0gg" target="_blank">s0gg</a></li>
-            <li>Blog: <a href="/posts">/posts</a></li>
-          </ul>
-        </section>
-        <section>
-          <h2>Likes</h2>
-          <p>Rust, Lisp, CTF, Neovim, Emacs</p>
-        </section>
-      </main>
+      <div class="screen">
+        <div class="screen-content">
+          <header>
+            <h1>s0gg</h1>
+          </header>
+          <main>
+            <section>
+              <p>Software engineer.</p>
+            </section>
+            <section class="links">
+              <ul>
+                <li>GitHub: <a href="https://github.com/s0gg" target="_blank">s0gg</a></li>
+                <li>Blog: <a href="/posts">/posts</a></li>
+              </ul>
+            </section>
+            <section>
+              <p>Likes: Rust, Lisp, CTF, Neovim, Emacs</p>
+            </section>
+          </main>
+        </div>
+      </div>
     </>
   );
 }
